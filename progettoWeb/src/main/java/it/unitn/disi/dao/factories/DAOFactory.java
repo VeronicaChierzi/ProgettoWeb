0736eb47d5157a4ba169f25956dff58155143a9f
root <root@ddlucad96-laptop>
2017-05-23 16:33:24 +0200

    Added dao support

---
+++ progettoWeb/src/main/java/it/unitn/disi/dao/factories/DAOFactory.java
@@ -0,0 +1,44 @@
+/*
+ * AA 2016-2017
+ * Introduction to Web Programming
+ * Common - DAO
+ * UniTN
+ */
+package it.unitn.disi.dao.factories;
+
+import it.unitn.disi.dao.exceptions.DAOFactoryException;
+import it.unitn.disi.dao.DAO;
+
+
+/**
+ * This interface must be implemented by all the concrete {@code DAOFactor(y)}
+ * ies.
+ * 
+ * @author Stefano Chirico &lt;stefano dot chirico at unitn dot it&gt;
+ * @since 2017.04.17
+ */
+public interface DAOFactory {
+
+    /**
+     * Shutdowns the access to the storage system.
+     * 
+     * @author Stefano Chirico
+     * @since 1.0.170417
+     */
+    public void shutdown();
+    
+    /**
+     * Returns the concrete {@link DAO dao} which type is the class passed as
+     * parameter.
+     * 
+     * @param <DAO_CLASS> the class name of the {@code dao} to get.
+     * @param daoInterface the class instance of the {@code dao} to get.
+     * @return the concrete {@code dao} which type is the class passed as
+     * parameter.
+     * @throws DAOFactoryException if an error occurred during the operation.
+     * 
+     * @author Stefano Chirico
+     * @since 1.0.170417
+     */
+    public <DAO_CLASS extends DAO> DAO_CLASS getDAO(Class<DAO_CLASS> daoInterface) throws DAOFactoryException;
+}
